41ffb4b856ea530da9e443e75d9b7d5932de40c5
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2025-09-22 10:37:31 +0200

    .

--- 9js.js
+++ 9js.js
@@ -1,22 +1,17 @@
 //9. f.
 let set1 = new Set([1,2,3,4])
 let set2 = new Set([3,4,5,6])
+
 function metszet(set1, set2)
 {
     let eredmeny = new Set();
     let tomb1 = Array.from(set1)
-    for (let i = 0; i < set1.length; i++)
-        {
-            for (let j = 0; j < set2.length; j++)
-            {
-                if (set1[i] == set2[j]){
-                    eredmeny.add(set1[i]) }
-            }
-        }  
-    for (let i = 0; i < eredmeny.length; i++)
+    for (let i= 0; i < tomb1.length; i++)
     {
-        return eredmeny[i]
+        if (set2.has(tomb1[i]))eredmeny.add(tomb1[i])
     }
+     return eredmeny
+
 }
 
 console.log(metszet(set1,set2))
