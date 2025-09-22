2000f4f30313cb491637c39408b08f85d125fc53
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2025-09-22 11:23:58 +0200

    .

--- 10js.js
+++ 10js.js
@@ -1,6 +1,16 @@
 let szo = prompt("Szó: ")
+let reverse = szo.split("").reverse().join("").toLowerCase().replace(/\s/g, "")
+
 function palindrom(szo)
 {
-    if (szo = palindrom) return true
-    else false
+    
+    let reverse = szo.split("").reverse().join("").toLowerCase().replace(/\s/g, "")
+    if (reverse == szo.split("").join("").toLowerCase().replace(/\s/g, "")) return "Igaz"
+    else return "Hamis"
+
+
 }
+
+console.log("Odafele: " + szo);
+console.log("Visszafele: " + reverse);
+console.log("Palindrom-e?: " + palindrom(szo));
